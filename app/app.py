e44960c712cae66ebc569635690c58f863710280
OotsukaEri <270299u@ugs.kochi-tech.ac.jp>
2025-09-16 16:39:10 +0900

    編集中

--- app/app.py
+++ app/app.py
@@ -4,8 +4,11 @@ app.py
 共通受付システムを想定
 エンドポイントへのルーティングを定義する
 """
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session,send_file
+import io
+import pandas as pd
 from subSystems.item import close_itemdb, init_itemdb, Item, Category
+from subSystems.user import close_userdb, init_userdb,User
 
 # Flaskアプリ初期化
 app = Flask(__name__)
@@ -91,7 +94,74 @@ def delete_item():
             return redirect("/")
         else:
             return render_template("p007_2.html", item=delete_item)
+#===================================================================
+#利用者登録
+@app.route('/register_user', methods=['GET','POST'])
+def register_user():
+    if request.method == 'GET':
+        return render_template("P003-1_userregister.html", errors=[])
+    else:
+        studentID = request.form['studentID']
+        password = request.form['password']
+        authority = request.form.get('authority', 1)  # デフォルト一般ユーザ
+        errors = User.check(studentID, password)
+        if errors:
+            return render_template("P003-2_userregistererror.html", errors=errors)
+        User.insert(studentID, password, authority)
+        return redirect(url_for('login'))
+    
+#===================================================================
+#ログイン
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("P001-1_login.html", error=None)
+    else:
+        studentID = request.form['studentID']
+        password = request.form['password']
+        user = User.authenticate(studentID, password)
+        if user:
+            session['user_id'] = user.id
+            session['studentID'] = user.studentID
+            session['authority'] = user.authority
+            
+            if user.authority == 0:
+                #管理者
+                return redirect(url_for('P001-2_home.html'))
+            else:
+                return redirect(url_for('P001-3_home.html'))
+        else:
+            return render_template("P001-4_loginerror.html", error="学籍番号またはパスワードが違います")
 
+#===================================================================
+# エクスポート（CSV）
+@app.route('/export_items')
+def export_items():
+    #ログインしているかの判定
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    items = Item.get_all()#Itemクラスを使ってすべての備品情報を取得
+
+    # 3. DataFrame に変換
+    df = pd.DataFrame([{
+        '備品ID': i['item_id'],
+        '備品名': i['name'],
+        'カテゴリ':i['category_id'],
+        '登録者': i['registrant_id'],
+        '登録日':i[''],
+        '備考': i['remark']
+    } for i in items])
+    
+    output = io.StringIO()#空のメモリ上の文字列バッファを作成
+    df.to.csv(output,index=False,enconding='utf8')
+    output.seek(0)
+    return send_file(
+        output,
+        mimetype='text/csv',
+        as_attachment=True,
+        download_name='items.csv'
+    )  
 #===================================================================
 
 ### ===== アプリ実行 ===== ###
